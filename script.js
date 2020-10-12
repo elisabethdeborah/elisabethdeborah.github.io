418714ae3101f676d36107d61cebd4271c7aff8c
elisabethdeborah <elisabeth_deborah@hotmail.com>
2020-10-12 16:12:42 +0200

    fixat länkar etc

--- script.js
+++ script.js
@@ -2,6 +2,8 @@ const nav = document.querySelector('nav');
 const navicon = document.querySelector('.navicon')
 const dropdown = document.querySelector('.dropdown')
 const projects = document.querySelector('.navbar__listitem__my-projects-rubrik')
+const screenshot = document.querySelectorAll('.project-cards-container__card')
+const screenshotContainer = document.querySelector('.project-cards-container')
 
 navicon.addEventListener('click', (event) => {
     nav.classList.toggle('synlig');
@@ -12,8 +14,22 @@ navicon.addEventListener('click', (event) => {
     }
 })
 
-projects.addEventListener('click', (event) => {
-    projects.classList.toggle('open');
-    dropdown.classList.toggle('visa-dropdown');
+projects.addEventListener('mouseover', (event) => {
+    projects.classList.add('open');
+    dropdown.classList.add('visa-dropdown');
+})
+
+projects.addEventListener('mouseout', (event) => {
+    projects.classList.remove('open');
+    dropdown.classList.remove('visa-dropdown');
+})
+
+screenshot.forEach(kort => {
+    kort.addEventListener('click', (event) => {
+        if(window.innerWidth>600) {
+        kort.classList.toggle('large');
+        screenshotContainer.classList.toggle('container-large');
+         }
+    })
 })
 
