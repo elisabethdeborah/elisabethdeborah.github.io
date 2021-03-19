2eea0ab31c775a7cf6a767940d63170d0243daa4
elisabethdeborah <elisabeth_deborah@hotmail.com>
2021-03-19 15:42:41 +0100

    .

--- flickr/moduler/main.js
+++ flickr/moduler/main.js
@@ -1,6 +1,6 @@
 //IMPORT FRÅN FILTER-FUNKTIONER.JS
 
-import {sorteraPopularitet, sorteraType, sorteraDatum, sorteraMiljö} from './filter-functions';
+import {sorteraPopularitet, sorteraType, sorteraDatum, sorteraMiljö} from './filter-functions.js';
 
 const sorteraEfterDatumFallande = document.getElementById('filter-date-descending');
 const sorteraEfterDatumStegrande = document.getElementById('filter-date-ascending');
