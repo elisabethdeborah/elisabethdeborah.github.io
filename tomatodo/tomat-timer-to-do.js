08ea99bd5f2b756f67787a6e5ee71e0ef3c60f59
Elisabeth Rumar <elisabeth_deborah@hotmail.com>
2022-05-27 20:28:49 +0200

    Updates

--- tomatodo/tomat-timer-to-do.js
+++
@@ -1,8 +0,0 @@
-
-const navicon = document.querySelector('.navicon');
-navicon.addEventListener('click', (event) => {
-    console.log('funkar');
-    header.classList.toggle('open');
-})
-
-const tomatoForm=document.querySelector('#tomato-form');
